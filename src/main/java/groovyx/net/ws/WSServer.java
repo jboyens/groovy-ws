87ca18112c343ed98c01fda510406a2da3136830
galleon <galleon@f908d50e-8e36-0410-ac0a-93754bd5008d>
2007-12-15 08:08:31 +0000

    Reformatting

--- src/main/java/groovyx/net/ws/WSServer.java
+++ src/main/java/groovyx/net/ws/WSServer.java
@@ -1,28 +1,22 @@
 package groovyx.net.ws;
 
+import groovy.lang.GroovyClassLoader;
+import org.apache.cxf.aegis.databinding.AegisDatabinding;
+import org.apache.cxf.frontend.ServerFactoryBean;
+
+import java.security.KeyStoreException;
 import java.util.HashMap;
 import java.util.Map;
-
-import groovy.lang.GroovyClassLoader;
-
-import org.apache.cxf.aegis.databinding.AegisDatabinding;
-import org.apache.cxf.configuration.security.AuthorizationPolicy;
-import org.apache.cxf.endpoint.Server;
-import org.apache.cxf.frontend.ServerFactoryBean;
-import org.apache.cxf.service.factory.AbstractServiceConfiguration;
 
 
 public class WSServer {
     private ServerFactoryBean sf;
 
-    public WSServer(AuthorizationPolicy ap) {
-    }
-    
-    public WSServer() {
+    public WSServer() throws KeyStoreException {
         sf = new ServerFactoryBean();
 
         Map<String,Object> props = new HashMap<String, Object>();
-//        props.put("mtom-enabled", Boolean.TRUE);      
+//      props.put("mtom-enabled", Boolean.TRUE);      
         sf.setProperties(props);
         
         sf.getServiceFactory().getServiceConfigurations().add(0, new GroovyConfiguration());
