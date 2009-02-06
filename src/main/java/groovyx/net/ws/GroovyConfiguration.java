70760ffd1b38aa82054cbf46ead0e765e6fe48dc
galleon <galleon@f908d50e-8e36-0410-ac0a-93754bd5008d>
2009-02-06 03:48:12 +0000

    Apply patch provided by Sergey Bondarenko to solve GMOD-54

--- src/main/java/groovyx/net/ws/GroovyConfiguration.java
+++ src/main/java/groovyx/net/ws/GroovyConfiguration.java
@@ -6,26 +6,42 @@ import org.apache.cxf.service.factory.AbstractServiceConfiguration;
 import org.apache.cxf.service.model.OperationInfo;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class GroovyConfiguration extends AbstractServiceConfiguration  {
-	
-	public Boolean isOperation(Method m) {
+    private static Method isSynthetic = null;
+    static {
+        try {
+            isSynthetic = Modifier.class.getDeclaredMethod("isSynthetic", Integer.TYPE);
+        } catch (NoSuchMethodException e) {
+            throw new Error(e);
+        }
+        isSynthetic.setAccessible(true);
+    }
 
-		try {
-			GroovyObject.class.getMethod(m.getName(), m.getParameterTypes());
-			return false;
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			return super.isOperation(m);
-		}
-	}
+    public Boolean isOperation(Method m) {
+        //skip all synthetic methods
+        try {
+            if ((Boolean)isSynthetic.invoke(null, m.getModifiers())) return false;
+        } catch (Exception e) {
+            throw new Error(e);
+        }
 
-	public String getAction(OperationInfo op, Method m) {
-		return op.getName().getLocalPart();
-	}
+        try {
+          GroovyObject.class.getMethod(m.getName(), m.getParameterTypes());
+          return false;
+        } catch (SecurityException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+          return false;
+        } catch (NoSuchMethodException e) {
+          // TODO Auto-generated catch block
+          return super.isOperation(m);
+        }
+    }
+
+    public String getAction(OperationInfo op, Method m) {
+        return op.getName().getLocalPart();
+    }
 
 }
